2cd40b4ed054db025b81f4892b13c5490f87cf36
Nipun <1.madvoid@gmail.com>
2013-12-26 01:15:04 -0700

    Commented and understood Blink.c

--- Blink/blink.c
+++ Blink/blink.c
@@ -6,6 +6,8 @@
 //
 // Credits:
 // 	Modified from the tutorial found at http://recursive-labs.com/blog/2012/10/28/stellaris-launchpad-gnu-linux-getting-started/
+//	I believe they modified it from the TivaWare Project0. See more at
+//	http://processors.wiki.ti.com/index.php/Stellaris_LM4F120_LaunchPad_Blink_the_RGB
 //
 // Requirements:
 // 	Requires Texas Instruments' TivaWare. Specifically, startup_gcc.c and any linker file (blinky.ld will work)
@@ -14,32 +16,41 @@
 // 	Basic blink program to use for learning and environment setup
 //
 // Notes:
-//	This code will be unnecessarily heavily commented for my own learning purposes
+//	This code will be unnecessarily heavily commented for my own learning purposes. The binary 
+//	file produced by this version of the code is 976 bytes.
 //****************************************************************************************************
 
+// Standard libraries
 #include <stdint.h>
 #include <stdbool.h>
-#include "inc/hw_gpio.h"
-#include "inc/hw_memmap.h"
-#include "inc/hw_sysctl.h"
-#include "inc/hw_types.h"
-#include "driverlib/gpio.h"
-#include "driverlib/rom.h"
-#include "driverlib/sysctl.h"
 
+// Anything from the "inc" folder has definitions - Registers, bit fields, etc.
+#include "inc/hw_types.h"	// Used for compilation of driverlibs. Not necessary for this project but included for ?safety?
+#include "inc/hw_memmap.h"	// Memory map	
+
+// Anything from the "driverlib" folder has the actual functions (drivers)
+#include "driverlib/rom.h"
+#include "driverlib/gpio.h"	// Only included for definitions since all functions are called from ROM
+#include "driverlib/sysctl.h"	// Only included for definitions since all functions are called from ROM
+
+// GPIO_PIN_X is defined in gpio.h
 #define LED_RED GPIO_PIN_1
 #define LED_BLUE GPIO_PIN_2
 #define LED_GREEN GPIO_PIN_3
 
 int main(){
+
+	// System clock set to run at 50 MHz from PLL with crystal ref.
+	// With EK-TM4C123GXL Launchpad, no need to change SYSCTL_XTAL_16MHZ to anything else
 	ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
 	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 	ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
 
-	for(;;){
-		ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_RED|LED_BLUE);
-		ROM_SysCtlDelay(5000000);
+	while(1){
+		ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, LED_RED|LED_GREEN|LED_BLUE);
+		ROM_SysCtlDelay(8333333);	// Number of loop iterations to perform @ 3 cycles/loop. Input = (DesiredTime*Frequency)/3
 		ROM_GPIOPinWrite(GPIO_PORTF_BASE, LED_RED|LED_GREEN|LED_BLUE, 0);
-		ROM_SysCtlDelay(5000000);
+		ROM_SysCtlDelay(8333333);
 	}
 }
+
