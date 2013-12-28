cdc82e4902ea51cb712684a2fad11b45d86b5979
Nipun <1.madvoid@gmail.com>
2013-12-28 01:07:47 -0700

    Added empty print project

---
+++ Print/print.c
@@ -0,0 +1,40 @@
+// blink.c
+//
+//****************************************************************************************************
+// Author:
+// 	Nipun Gunawardena
+//
+// Credits:
+//	Modified from the TivaWare 'hello' program
+//
+// Requirements:
+// 	Requires Texas Instruments' TivaWare.
+//
+// Description:
+// 	Basic serial print program for rudimentary debugging learning
+//
+// Notes:
+//
+//****************************************************************************************************
+
+// Includes
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+// Defines
+#define LED_RED GPIO_PIN_1
+#define LED_BLUE GPIO_PIN_2
+#define LED_GREEN GPIO_PIN_3
+
+int main(){
+
+}
+
