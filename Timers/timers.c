7399fcf34efa7b72b640bc07d3664f474beb1506
Nipun <1.madvoid@gmail.com>
2014-01-07 01:54:44 -0700

    Added empty Timers project

---
+++ Timers/timers.c
@@ -0,0 +1,78 @@
+// timers.c
+//
+//****************************************************************************************************
+// Author:
+// 	Nipun Gunawardena
+//
+// Credits:
+//	Modified from the TivaWare 'timers' program
+//
+// Requirements:
+// 	Requires Texas Instruments' TivaWare.
+//
+// Description:
+// 	Basic timers program for learning.
+//
+// Notes:
+//	Keep interrupts as short as possible
+//
+//****************************************************************************************************
+
+
+// Includes ------------------------------------------------------------------------------------------
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+
+#include "utils/uartstdio.h"
+
+
+
+// Defines -------------------------------------------------------------------------------------------
+#define LED_RED GPIO_PIN_1
+#define LED_BLUE GPIO_PIN_2
+#define LED_GREEN GPIO_PIN_3
+
+
+
+
+// Functions -----------------------------------------------------------------------------------------
+
+void ConfigureUART(void){
+
+	// Enable the peripherals used by UART
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+	// Set GPIO A0 and A1 as UART pins.
+	GPIOPinConfigure(GPIO_PA0_U0RX);
+	GPIOPinConfigure(GPIO_PA1_U0TX);
+	ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+	// Configure the UART for 115,200, 8-N-1 operation.
+	//ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+        // Configure UART clock using UART utils. The above line does not work by itself to enable the UART. The following two
+	// lines must be present. Why?
+        UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+        UARTStdioConfig(0, 115200, 16000000);
+}
+
+
+
+
+// Main ----------------------------------------------------------------------------------------------
+int main(void){
+
+}
+
