b1a508b63434594739ba08210f8397fcf1a40edf
Nipun <1.madvoid@gmail.com>
2014-04-04 02:48:52 -0600

    BMP180 calibration working

--- BMP180/bmp180.c
+++ BMP180/bmp180.c
@@ -45,11 +45,116 @@
 #define LED_BLUE GPIO_PIN_2
 #define LED_GREEN GPIO_PIN_3
 
+// BMP180 Registers & Commands
+#define BMP180_I2C_ADDRESS 0x77
+#define BMP180_READ_TEMP 0x2E
+#define BMP180_READ_PRES_BASE 0x34
+#define BMP180_REG_CAL_AC1 0xAA
+#define BMP180_REG_CAL_AC2 0xAC
+#define BMP180_REG_CAL_AC3 0xAE
+#define BMP180_REG_CAL_AC4 0xB0
+#define BMP180_REG_CAL_AC5 0xB2
+#define BMP180_REG_CAL_AC6 0xB4
+#define BMP180_REG_CAL_B1  0xB6
+#define BMP180_REG_CAL_B2  0xB8
+#define BMP180_REG_CAL_MB  0xBA
+#define BMP180_REG_CAL_MC  0xBC
+#define BMP180_REG_CAL_MD  0xBE
+#define BMP180_REG_CHIPID  0xD0
+#define BMP180_REG_VERSION 0xD1
+#define BMP180_REG_SOFTRESET 0xE0
+#define BMP180_REG_CONTROL 0xF4
+#define BMP180_REG_TEMPDATA 0xF6
+#define BMP180_REG_PRESSUREDATA 0xF6
+
+
 
 // Variables -----------------------------------------------------------------------------------------
+uint8_t g_oversamplingSetting = 3;
+uint8_t g_calRegs[] = {BMP180_REG_CAL_AC1, BMP180_REG_CAL_AC2, BMP180_REG_CAL_AC3, BMP180_REG_CAL_AC4, BMP180_REG_CAL_AC5,  BMP180_REG_CAL_AC6,  BMP180_REG_CAL_B1,  BMP180_REG_CAL_B2,  BMP180_REG_CAL_MB,  BMP180_REG_CAL_MC,  BMP180_REG_CAL_MD };
+uint32_t g_calRawVals[22];
+
+struct {
+      int16_t  ac1;
+      int16_t  ac2;
+      int16_t  ac3;
+      uint16_t ac4;
+      uint16_t ac5;
+      uint16_t ac6;
+      int16_t  b1;
+      int16_t  b2;
+      int16_t  mb;
+      int16_t  mc;
+      int16_t  md;
+} g_Bmp180CalibData;
+
 
 
 // Functions -----------------------------------------------------------------------------------------
+void BMP180GetCalVals(){
+	for(int i = 0; i < 11; i++){
+		// Configure to write, set register to send, send
+		ROM_I2CMasterSlaveAddrSet(I2C3_BASE, BMP180_I2C_ADDRESS, false);
+		ROM_I2CMasterDataPut(I2C3_BASE, g_calRegs[i]);
+		ROM_I2CMasterControl(I2C3_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+
+		// Wait for bus to free
+		while(ROM_I2CMasterBusy(I2C3_BASE)){}
+
+		// Send restart
+		ROM_I2CMasterSlaveAddrSet(I2C3_BASE, BMP180_I2C_ADDRESS, true);
+
+		// Read MSB
+		ROM_I2CMasterControl(I2C3_BASE, I2C_MASTER_CMD_BURST_RECEIVE_START);
+		while(ROM_I2CMasterBusy(I2C3_BASE)){}
+		g_calRawVals[2*i] = ROM_I2CMasterDataGet(I2C3_BASE);
+		
+		// Read LSB
+		ROM_I2CMasterControl(I2C3_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH);
+		while(ROM_I2CMasterBusy(I2C3_BASE)){}
+		g_calRawVals[2*i+1] = ROM_I2CMasterDataGet(I2C3_BASE);
+	}
+	g_Bmp180CalibData.ac1 = (int16_t) ( (g_calRawVals[0] << 8) | g_calRawVals[1] );
+	g_Bmp180CalibData.ac2 = (int16_t) ( (g_calRawVals[2] << 8) | g_calRawVals[3] );
+	g_Bmp180CalibData.ac3 = (int16_t) ( (g_calRawVals[4] << 8) | g_calRawVals[5] );
+	g_Bmp180CalibData.ac4 = (uint16_t)( (g_calRawVals[6] << 8) | g_calRawVals[7] );
+	g_Bmp180CalibData.ac5 = (uint16_t)( (g_calRawVals[8] << 8) | g_calRawVals[9] );
+	g_Bmp180CalibData.ac6 = (uint16_t)( (g_calRawVals[10] << 8) | g_calRawVals[11] );
+	g_Bmp180CalibData.b1 =  (int16_t) ( (g_calRawVals[12] << 8) | g_calRawVals[13] );
+	g_Bmp180CalibData.b2 =  (int16_t) ( (g_calRawVals[14] << 8) | g_calRawVals[15] );
+	g_Bmp180CalibData.mb =  (int16_t) ( (g_calRawVals[16] << 8) | g_calRawVals[17] );
+	g_Bmp180CalibData.mc =  (int16_t) ( (g_calRawVals[18] << 8) | g_calRawVals[19] );
+	g_Bmp180CalibData.md =  (int16_t) ( (g_calRawVals[20] << 8) | g_calRawVals[21] );
+	
+}
+
+void BMP180GetRawTemp(){
+	// Configure to write, send control register
+	ROM_I2CMasterSlaveAddrSet(I2C3_BASE, BMP180_I2C_ADDRESS, false);
+	ROM_I2CMasterDataPut(I2C3_BASE, BMP180_REG_CONTROL);
+	ROM_I2CMasterControl(I2C3_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+
+	// Wait for bus to free
+	while(ROM_I2CMasterBusy(I2C3_BASE)){}
+
+	// Send temperature command
+	ROM_I2CMasterDataPut(I2C3_BASE, BMP180_READ_TEMP);
+	ROM_I2CMasterControl(I2C3_BASE, I2C_MASTER_CMD_BURST_SEND_CONT);
+
+	// Wait for bus to free
+	while(ROM_I2CMasterBusy(I2C3_BASE)){}
+
+	// Delay for 4.5 ms
+	ROM_SysCtlDelay(ROM_SysCtlClockGet()/3/222);
+
+	// Send restart
+	ROM_I2CMasterSlaveAddrSet(I2C3_BASE, BMP180_I2C_ADDRESS, true);
+}
+
+void BMP180GetRawPressure(){
+
+}
+
 void ConfigureUART(void){
 
 	// Enable the peripherals used by UART
@@ -108,6 +213,7 @@ void FloatToPrint(float floatValue, uint32_t splitValue[2]){
 
 
 
+
 // Main ----------------------------------------------------------------------------------------------
 int main(void){
 
@@ -119,7 +225,7 @@ int main(void){
 
 	// Initialize the UART and write status.
 	ConfigureUART();
-	UARTprintf("SHT21 Example\n");
+	UARTprintf("BMP180 Example\n");
 
 	// Enable LEDs
 	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
@@ -127,6 +233,22 @@ int main(void){
 
 	// Enable I2C3
 	ConfigureI2C3(true);
+
+	// Check oversampling
+	if(g_oversamplingSetting > 3) g_oversamplingSetting = 3;
+
+	BMP180GetCalVals();
+	UARTprintf("AC1: %i\n", g_Bmp180CalibData.ac1);
+	UARTprintf("AC2: %i\n", g_Bmp180CalibData.ac2);
+	UARTprintf("AC3: %i\n", g_Bmp180CalibData.ac3);
+	UARTprintf("AC4: %i\n", g_Bmp180CalibData.ac4);
+	UARTprintf("AC5: %i\n", g_Bmp180CalibData.ac5);
+	UARTprintf("AC6: %i\n", g_Bmp180CalibData.ac6);
+	UARTprintf("B1: %i\n", g_Bmp180CalibData.b1);
+	UARTprintf("B2: %i\n", g_Bmp180CalibData.b2);
+	UARTprintf("MB: %i\n", g_Bmp180CalibData.mb);
+	UARTprintf("MC: %i\n", g_Bmp180CalibData.mc);
+	UARTprintf("MD: %i\n", g_Bmp180CalibData.md);
 
 	while(1){
 		// Blink LED
